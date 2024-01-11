c507255016d2977a0175240519906ada841f337c
keanu.klenner <keanu.klenner@connectiv.de>
2024-01-11 15:13:25 +0100

    commit

---
+++ ConneThemeName/src/ThemeName.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Conne\ThemeName;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class ThemeName extends Plugin implements ThemeInterface
+{
+}
+
